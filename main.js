c02449ce8a2b076513408d8ad92a9fcc950df938
david ortega <david.ortega.dev@gmail.com>
2019-12-20 12:24:15 -0600

    very minor changes(smaller headers on list)

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // CONSTRUCTS COFFEE LIST
 function renderCoffee(coffee) {
 	var html = '<li class="coffee-list-item" id='+coffee.id+'>';
-	html += '<h2>' + coffee.name + '</h2>'
+	html += '<h3>' + coffee.name + '</h3>'
 		+ '<p>' + coffee.roast +'</p>';
 	html += '</li>';
 	return html;
@@ -41,7 +41,7 @@ document.getElementById("coffee").addEventListener("click",function(e) {
 			'<img src="'+coffees[e.target.id-1].img+'" alt="" style="height:400px;">'+
 			"<h1>"+coffees[e.target.id-1].name+"</h1>"+
 			'<p>'+coffees[e.target.id-1].flavorText+'</p>';
-		//second attempt to create tabs on click
+		//create tabs on click
 		if(document.getElementsByClassName("active-list-item").length===0){
 			document.getElementById(e.target.id).classList.add("active-list-item");
 		} else {
@@ -131,7 +131,8 @@ function addToCoffees() {
 		{id: 3, name:'Do The Work Blend', roast:'light', img:'img/imgDoTheWork.jpg', flavorText:'Cupping notes: caramel, citrus, slight fruit and fuzzy feelings that come from doing good',},
 		{id: 4, name:'Driveway Blend', roast:'light', img:'img/imgDriveway.jpg', flavorText:'The blend itself boldly features Ethiopian and Sumatran offerings that add some punch to this medium body coffee. ',},
 		{id: 5, name:'Espresso Blend', roast:'light', img:'img/imgEspresso.png', flavorText:'Great citrus pumps through in the espresso brewing process with strong body and nutty crema. Often used to brew a solid cup through other methods.',},
-		{id: 6, name:'Heywood Hotel Blend', roast:'medium', img:'img/imgHeywood.jpg', flavorText:'This blend was created exclusively for our long-time partners at the Heywood Hotel in East Austin. Their coffee is as unique as their award-winning boutique hotel and provides the same attention to detail they are known for. Many guests ask how to procure the coffee once they leave Austin so the Heywood team has decided to make it available to you!\n' + '\n' +
+		{id: 6, name:'Heywood Hotel Blend', roast:'medium', img:'img/imgHeywood.jpg', flavorText:'This blend was created exclusively for our long-time partners at the Heywood Hotel in East Austin. Their coffee is as unique as their award-winning ' +
+				'boutique hotel and provides the same attention to detail they are known for. Many guests ask how to procure the coffee once they leave Austin so the Heywood team has decided to make it available to you!\n' + '\n' +
 				'Cupping notes: Gently acidic with strong cacao and nuttiness with slight berry/citrus',},
 		{id: 7, name:'House Blend', roast:'medium', img:'img/imgHouse.png', flavorText:'Full bodied smokiness from a balanced medium roast. Lightly sweet berries with a crisp finish.',},
 		{id: 8, name:'It\'s A Boy Blend', roast:'medium', img:'img/imgBoy.jpg', flavorText:'Celebrate the birth of a new baby boy with a gift everyone loves, coffee!\n' + '\n' + 'This classic blend provides a solid body and smooth finish.\n' + '\n' + 'Congrats!',},
@@ -141,8 +142,10 @@ function addToCoffees() {
 		{id: 12, name:'Sleep Slayer Blend', roast:'dark', img:'img/imgSlayer.jpg', flavorText:'Kick your morning in the teeth! Bold falls short of describing this punchy mix of Organic Mexican and Ethiopian powerhouses. Full bodied coffee that surprises you with a slight citrus finish and a mouthful of stonefruit',},
 		{id: 13, name:'Ethiopian Single Origin', roast:'dark', img:'img/imgEthiopian.png', flavorText:'The Harrar region lies in the Eastern Highlands of Ethiopia. As one of the main growing regions in Ethiopia, Harrar has a reputation for producing distinctive wild-varietal Arabica.\n' + '\n' +
 				'Cupping notes: heavy berry, lemon, stonefruit, dark chocolate, typical moka flavor, marked blueberry and blackberry\n' + '\n' + 'Heavy body but fair to light acidity ',},
-		{id: 14, name:'Guatemalan Single Origin', roast:'dark', img:'img/imgGuatemalan.png', flavorText:'This type comes from the rich volcanic soils of the Santa Rosa valleys located in the South Central region of Guatemala. The Central Highlands of the country are ideal for coffee cultivation due to high altitudes and rich soil from surrounding volcanoes. The climate allows the coffee to mature slowly, which is said to concentrate the coffee beans flavors. The coffee grown in the Santa Rosa region boasts a balanced cup with chocolate notes.',},
-		{id: 15, name:'Sumatran Single Origin', roast:'dark', img:'img/imgSumatran.jpeg', flavorText:'The unique method used in its production results in a very full body with a concentrated flavor, garnished with herbal nuances and a spicy finish. Giling Basah, the name of the traditional Sumatran process, involves hulling the parchment off of the bean at roughly 50% moisture content; for comparison, most other processes hull coffee at around 10-12% moisture. This unique Sumatran process results in a trademark flavor profile (low acidity and a richness that lingers on the back of the palate) and gives the green beans a signature dark color. Notes of chocolate are evident in the finish.',},
+		{id: 14, name:'Guatemalan Single Origin', roast:'dark', img:'img/imgGuatemalan.png', flavorText:'This type comes from the rich volcanic soils of the Santa Rosa valleys located in the South Central region of Guatemala. ' +
+				'The Central Highlands of the country are ideal for coffee cultivation due to high altitudes and rich soil from surrounding volcanoes. The climate allows the coffee to mature slowly, which is said to concentrate the coffee beans flavors. The coffee grown in the Santa Rosa region boasts a balanced cup with chocolate notes.',},
+		{id: 15, name:'Sumatran Single Origin', roast:'dark', img:'img/imgSumatran.jpeg', flavorText:'The unique method used in its production results in a very full body with a concentrated flavor, garnished with herbal nuances and a spicy finish. Giling Basah, the name of the ' +
+				'traditional Sumatran process, involves hulling the parchment off of the bean at roughly 50% moisture content; for comparison, most other processes hull coffee at around 10-12% moisture. This unique Sumatran process results in a trademark flavor profile (low acidity and a richness that lingers on the back of the palate) and gives the green beans a signature dark color. Notes of chocolate are evident in the finish.',},
 		{id: 16, name:'Italian Single Origin', roast:'dark', img:'img/imgItalian.png', flavorText:'This dark chocolaty roast is full bodied with no acidity. You will notice a caramel sweetness similar to a creme brule. A tingling of anise and pepper will round your taste buds for an enjoyable finish. ',},
 	];
 
